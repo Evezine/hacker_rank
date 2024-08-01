e65af9869db0c759f79acffa99d339916405557b
viswa <viswa.r2002@gmail.com>
2024-08-01 12:22:16 +0530

    first commit

---
+++ swapping_two-no.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter the  num1:"))
+num2 = int(input("Enter the  num2:"))
+
+print("value of num 1 before swapping:",num1)
+print("value of num 2 before swapping:",num2)
+
+num1,num2=num2,num1
+
+print("value of num 1 after swapping:",num1)
+print("value of num 2 after swapping:",num2)
